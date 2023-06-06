4188862c86e406198d2440c366fbad865d7c0c73
Lecuyer Quentin <Quentin.Lecuyer@mailfence.com>
2023-06-06 11:24:13 +0200

    Modif pagination

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,10 @@
+
+
+
+ 
+
+
+
 const slides = [
 	{
 		"image":"./assets/images/slideshow/slide1.jpg",
@@ -16,6 +23,7 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
 const arrows = [
 	{
 		"image":"./assets/images/arrow_left.png",
@@ -32,16 +40,23 @@ class Carousel {
 	constructor(element) {
 		//* Element du DOM sur lequel on va attacher le carousel.
 		this.element = element
+
 		//* Creation des éléments du DOM.
 		this.root = this.createDivWithClass('carousel')
-		this.root.setAttribute('tabindex', '0')// Add a tabindex to the carousel.
+		//* Ajout d'un tabindex pour que le carousel soit focusable.
+		this.root.setAttribute('tabindex', '0')
+
+		//* Creation du container.
 		this.container = this.createDivWithClass('carousel-container')
+
 		//* Initialisation des variables.
 		this.currentSlide = 0
 		this.bullets = []
+
 		//* Ajout des éléments au DOM.
 		this.root.appendChild(this.container)
 		this.element.appendChild(this.root)
+
 		//* Appel des méthodes.
 		this.createCarouselSlides()
 		this.createNavigation()
@@ -56,21 +71,17 @@ class Carousel {
 	createCarouselSlides() {
 		this.carouselSlides = this.createDivWithClass('carousel-slides')
 		this.container.appendChild(this.carouselSlides)
+
 		//* Boucle sur le tableau slides pour créer les slides.
   		slides.forEach(slide => {
 			this.slideElement = this.createDivWithClass('carousel-slide')
-
-			//! A verifier si c'est mieux de faire comme ça ou comme en dessous.
 			this.slideElementImage = document.createElement('img')
 			this.slideElementImage.src = slide.image
 			this.slideElementImage.alt = 'image'
 			this.slideElement.appendChild(this.slideElementImage)
 			this.slideElementTagLine = document.createElement('p')
-			this.slideElementTagLine.innerHTML = slide.tagLine //! Pour que le span soit pris en compte. Verifier si faillible.
-			this.slideElement.appendChild(this.slideElementTagLine)
-
-    		// this.slideElement.innerHTML = `<img src="${slide.image}" alt="image"/> <p>${slide.tagLine}</p>`
-			
+			this.slideElementTagLine.innerHTML = slide.tagLine
+			this.slideElement.appendChild(this.slideElementTagLine)			
     		this.carouselSlides.appendChild(this.slideElement)
   		})
 	}
@@ -78,28 +89,33 @@ class Carousel {
 
 	//* Contrôle la largeur du carousel et des slides.
 	setStyle() {
+		//* Calcul de la largeur du carousel
 		const carouselWidth = slides.length * 100 //? Nombres de slide multiplié par 100.
 		this.container.style.width = `${carouselWidth}%` //? On ajoute le % pour que le style soit bien responsive.
+		
+		//* Calcul de la largeur des slides
 		const slideWidth = 100 / slides.length //? Nombres de slide divisé par 100
 		this.slideElement.style.width = `${slideWidth}%` //? On ajoute le % pour que le style soit bien responsive.
 	}
 
 	//* Creation des fleches de navigations et utilisation du clavier. */
 	createNavigation(){
-		//* Creation des boutons.
+		//* Creation du bouton Previous.
 		this.previousButton = this.createDivWithClass('carousel-prev')
 		//? AJout de l'image dans le bouton.
 		this.arrowLeft = document.createElement('img')
 		this.arrowLeft.src = arrows[0].image
 		this.arrowLeft.alt = 'arrow_left'
 		this.previousButton.appendChild(this.arrowLeft)
-		//* Creation des boutons.
+
+		//* Creation du bouton Next.
 		this.nextButton = this.createDivWithClass('carousel-next')
 		//? AJout de l'image dans le bouton.
 		this.arrowRight = document.createElement('img')
 		this.arrowRight.src = arrows[1].image
 		this.arrowRight.alt = 'arrow_right'
 		this.nextButton.appendChild(this.arrowRight)
+
 		//* Ajout des boutons au DOM.
 		this.root.appendChild(this.previousButton)
 		this.root.appendChild(this.nextButton)
@@ -107,6 +123,8 @@ class Carousel {
 		//* Ajouts des event listeners sur les fleches de navigation.
 		this.previousButton.addEventListener('click', this.previous.bind(this))
 		this.nextButton.addEventListener('click', this.next.bind(this))
+
+
 		//? Avec addEvenListener, this fait référence à l'élément sur lequel on a cliqué.
 		//? Mais avec bind(this) on fait référence à la classe Carousel. Donc on peut utiliser les méthodes de la classe.
 
@@ -122,21 +140,25 @@ class Carousel {
 
 	//* Creation de la pagination. */
 	createPagination() {
-		const pagination = this.createDivWithClass('carousel-pagination')
-		this.root.appendChild(pagination)
+		//* Creation de la div qui contiendra les bullets.
+		this.pagination = this.createDivWithClass('carousel-pagination')
+		this.root.appendChild(this.pagination)
+
 		//* Boucle sur les slides pour créer les bullets.
 		slides.forEach((slide, index) => {
 			//* Creation des bullets.
-			const bullet = this.createDivWithClass('carousel-bullet')
-			pagination.appendChild(bullet)
-			this.bullets.push(bullet)
+			this.bullet = this.createDivWithClass('carousel-bullet')
+			this.pagination.appendChild(this.bullet)
+			this.bullets.push(this.bullet)
+
 			//* Ajout de l'event listener sur les bullets.
-			bullet.addEventListener('click', () => {
+			this.bullet.addEventListener('click', () => {
 				this.currentSlide = index
 				this.moveCarouselTo(this.currentSlide)
 				this.activeBullet()
 			})
 		})
+		this.activeBullet() //? Appel de la méthode activeBullet pour que le premier bullet soit actif au chargement de la page.
 	}
 
 	/**
@@ -144,8 +166,13 @@ class Carousel {
 	 * @param {number} currentSlide 
 	 */
 	moveCarouselTo(currentSlide) {
-		const transformValue = `translateX(-${currentSlide * (100 / slides.length)}%)`//?Le slide se déplace de la largueur du slide * l'index du slide courant. Ce qui permet de déplacer le slide en fonction de l'index. En négatif pour que le slide se déplace vers la gauche.
-		this.carouselSlides.style.transform = transformValue //? On applique la valeur de transformValue à la propriété transform du carouselSlides.
+		//? Le slide se déplace de la largueur du slide * l'index du slide courant. 
+		//? Ce qui permet de déplacer le slide en fonction de l'index.
+		//? En négatif pour que le slide se déplace vers la gauche.
+		//? En pourcentage pour que le carousel soit responsive.
+		const transformValue = `translateX(-${currentSlide * (100 / slides.length)}%)`
+		//? On applique la valeur de transformValue à la propriété transform du carouselSlides.
+		this.carouselSlides.style.transform = transformValue 
 	}
 
 	//* Go to the previous slide.
@@ -155,11 +182,11 @@ class Carousel {
 			this.currentSlide = slides.length - 1 //? On revient au dernier slide.
 		}
 		if(this.currentSlide >= 0){//? Si l'index du slide courant est supérieur ou égal à 0.
-			this.activeBullet()
+			this.activeBullet() //? On active le bullet correspondant au slide courant.
 		}
-		this.moveCarouselTo(this.currentSlide)
-		
+		this.moveCarouselTo(this.currentSlide) //? On déplace le carousel vers le slide courant.
 	}
+
 	//* Go to the next slide.
 	next() {
 		this.currentSlide++ //? On incrémente l'index du slide courant.
@@ -167,9 +194,9 @@ class Carousel {
 			this.currentSlide = 0 //? On revient au premier slide.
 		}
 		if(this.currentSlide < slides.length){//? Si l'index du slide courant est inférieur au nombre de slide.
-			this.activeBullet()
+			this.activeBullet() //? On active le bullet correspondant au slide courant.
 		}
-		this.moveCarouselTo(this.currentSlide)
+		this.moveCarouselTo(this.currentSlide) //? On déplace le carousel vers le slide courant.
 	}
 
 	//* Active the current bullet.
@@ -177,6 +204,7 @@ class Carousel {
 		this.bullets.forEach(bullet => bullet.classList.remove('carousel-bullet-active'))
 		this.bullets[this.currentSlide].classList.add('carousel-bullet-active')
 	}
+
 	/** 
 	 * * Create a div with a given class. *
 	 * @param {string} className 
@@ -190,6 +218,9 @@ class Carousel {
 }
 
 //? On attend que le DOM soit chargé pour créer le carousel.
+
 document.addEventListener('DOMContentLoaded', function() {
 	new Carousel(document.querySelector('#banner'))
 })
+
+
